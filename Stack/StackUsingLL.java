3a07227f89109b4999b0dbe6f62281027c6d71c0
Devansh-Khandor <devanshkhandor1234@gmail.com>
2026-03-17 16:09:37 +0530

    Initial commit

---
+++ Stack/StackUsingLL.java
@@ -0,0 +1,58 @@
+package Stack;
+
+public class StackUsingLL {
+
+    public static class Node{
+        Node next;
+        int data;
+        Node(int data){
+            this.next = null;
+            this.data = data;
+        }
+    }
+
+    static class Stack{
+        public static Node head;
+
+        public static boolean isEmpty(){
+            return head==null;
+        }
+        public static void push(int x){
+            Node newNode = new Node(x);
+            if(isEmpty()){
+                head = newNode;
+                return;
+            }
+            newNode.next = head;
+            head = newNode;
+        }
+
+        public static int pop(){
+            if(isEmpty()){
+                return -1;
+            }
+            int top = head.data;
+            head = head.next;
+            return top;
+        } 
+
+        public static int peek(){
+            if(isEmpty()){
+                return -1;
+            }
+            return head.data;
+        }
+    }
+
+    public static void main(String args[]){
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        while(!s.isEmpty()){
+            System.out.print(s.peek()+" ");
+            s.pop();
+        }
+    }
+}
